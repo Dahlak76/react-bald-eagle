c63e8d0888bf2d157946cf37056742069db27435
dahlak76 <88516807+Dahlak76@users.noreply.github.com>
2022-10-28 01:38:15 -0500

    todo rendered

--- bald-eagle-1/src/App.js
+++ bald-eagle-1/src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
 
+const todoList = [
+  {
+    id: 1,
+    title: 'study',
+  },
+  {
+    id: 2,
+    title: 'sleep',
+  },
+  {
+    id: 3,
+    title: 'eat',
+  },
+];
+
 function App() {
   return (
     <div>
-      <h1>
-        hello world
-      </h1>
+      <hid>to do list</hid>
+      <ul>
+        {todoList.map((todo) => (
+          <li key={todo.id}>{todo.title}</li> 
+         ))}                             
+      </ul>
     </div>
   );
 }
